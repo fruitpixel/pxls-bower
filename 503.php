0ff49b49153f425571772ea4c1532e7da1c4d05b
Richard Howarth <r.howarth@airmaxgroup.com>
2014-10-01 14:36:02 +0100

    Change 503 message to be more generic

--- 503.php
+++ 503.php
@@ -2,14 +2,9 @@
 <html lang="en-US">
 <head>
 <meta charset="UTF-8" />
-<title>:PXLS:Themes</title>
-<link rel="stylesheet" type="text/css" href="http://pxls.co.uk/wp-content/themes/pxls/theme/holding.css" media="screen" />
+<title><?php bloginfo( 'name' ); ?> <?php wp_title(); ?></title>
 </head>
 <body class="">
-    <div class="holding-container">
-        <div class="holding-inner-container">                
-            <a href="mailto:hello@pxls.co.uk"><img src="http://pxls.co.uk/wp-content/themes/pxls/images/holding.png" alt=""></a>
-        </div>
-    </div>
+    <h1>Unavailable for maintenance - we'll be back shortly.</h1>
 </body>
 </html>
